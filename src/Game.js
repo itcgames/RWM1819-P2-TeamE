7f2ee9e651b2452b953e1819fb29c2e3b842f7ba
Brandon Seah-Dempsey <C00204076@itcarlow.ie>
2018-11-29 16:35:03 +0000

    Draws help icons

--- src/Game.js
+++ src/Game.js
@@ -41,7 +41,7 @@ class Game
      debugDraw.SetFlags(this.b2DebugDraw.e_shapeBit | this.b2DebugDraw.e_jointBit);
      this.world.SetDebugDraw(debugDraw);
 
-     this.audioManager = new AudioManager();
+     //this.audioManager = new AudioManager();
 
 	    for(var i = 0; i < 400; i++)
     		{
@@ -91,7 +91,7 @@ class Game
       //this.gestureManager.resetDetection()
 
     }
-  
+
 
     //this.AssetManager.update();
     //window.requestAnimationFrame(gameNs.game.update);
--- src/Menus/Help.js
+++ src/Menus/Help.js
@@ -15,7 +15,19 @@ class Help
     //this.playBtn = new AssetManager(200, 200, 500, 250, "mycanvas");
     this.playBtn = new Image()
 
+    this.dragcross = new Image();
+    this.eraser = new Image();
+    this.line = new Image();
+    this.mouse = new Image();
+    this.pencil = new Image();
+
     this.playBtn.src = "resources/img/main_button.png";
+
+    this.dragcross.src = "resources/img/drag_cross.png";
+    this.eraser.src = "resources/img/eraser.png";
+    this.line.src = "resources/img/line.png";
+    this.mouse.src = "resources/img/mouse.png";
+    this.pencil.src = "resources/img/pencil.png";
 
     this.startingPosition = []
     this.gestureManager = new GestureManager()
@@ -70,6 +82,12 @@ class Help
   //  this.playBtn.draw();
 
     ctx.drawImage(this.playBtn,750, 500, 200, 100);
+
+    ctx.drawImage(this.pencil, 90, 100, 70, 70);
+    ctx.drawImage(this.eraser, 90, 250, 70, 70);
+    ctx.drawImage(this.line, 90, 400, 70, 70);
+    ctx.drawImage(this.mouse, 620, 100, 70, 70);
+    ctx.drawImage(this.dragcross, 620, 250, 70, 70);
 
 
     //ctx.fillText(this.title, 100,100);
